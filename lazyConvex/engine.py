9fb4e8503651a7788359e4969c75546cda48d7e3
Nathan D'Addio <nathan.daddio@gmail.com>
2016-09-24 20:07:23 +1000

    Added warm start at root node in callback, refactored the way variable values are found

--- lazyConvex/engine.py
+++ lazyConvex/engine.py
@@ -10,6 +10,21 @@ import gurobipy
 
 TOLERANCE = 10**(-5)
 
+# Store away some of these nasty callback codes
+MIPSOL = gurobipy.GRB.Callback.MIPSOL
+MIPNODE = gurobipy.GRB.Callback.MIPNODE
+MIPNODE_STATUS = gurobipy.GRB.Callback.MIPNODE_STATUS
+NODE_COUNT = gurobipy.GRB.Callback.MIPNODE_NODCNT
+
+
+def get_value_lookup(model, where):
+    if where == MIPSOL:
+        return model.cbGetSolution
+    elif where == MIPNODE:
+        return model.cbGetNodeRel
+    else:
+        raise ValueError('{} is not a valid callback code'.format(where))
+
 
 class LazyConvexEngine(object):
     def __init__(self, model, objective_function, objective_variables):
@@ -34,18 +49,31 @@ class LazyConvexEngine(object):
                 self._approximation_callback(model, where)
             except:
                 traceback.print_exc()
+                raise
 
         self._model.optimize(callback)
 
     def _approximation_callback(self, model, where):
-        if where == gurobipy.GRB.Callback.MIPSOL:
-            approximation_value = model.cbGetSolution(self._approximation_variable)
+        root_node = (where == MIPNODE and
+                     model.cbGet(NODE_COUNT) == 0 and
+                     model.cbGet(MIPNODE_STATUS) == 2)
+        mip_sol = where == MIPSOL
 
-            objective_variable_values = model.cbGetSolution(self._objective_variables)
-            actual_value = self._objective_function.get_objective(objective_variable_values)
-
+        if mip_sol or root_node:
+            values = self._get_values(model, where)
+            approximation_value, objective_variable_values, actual_value = values
             if actual_value - approximation_value > TOLERANCE:
-                self._add_approximation(model, actual_value, objective_variable_values)
+                self._add_approximation(
+                    model, actual_value, objective_variable_values
+                )
+
+    def _get_values(self, model, where):
+        get_value = get_value_lookup(model, where)
+        approximation_value = get_value(self._approximation_variable)
+        objective_variable_values = get_value(self._objective_variables)
+        actual_value = self._objective_function.get_objective(objective_variable_values)
+
+        return approximation_value, objective_variable_values, actual_value
 
     def _add_approximation(self, model, actual_value, objective_variable_values):
         """
